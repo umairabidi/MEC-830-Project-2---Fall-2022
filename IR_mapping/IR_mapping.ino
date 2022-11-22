81fdda6179075f2e5151ed7c59b91519dfdc0549
Abhay Gupta <103391796+AbhayGupta-coder@users.noreply.github.com>
2022-11-21 22:59:00 -0500

    Update IR_mapping.ino

--- IR_mapping/IR_mapping.ino
+++ IR_mapping/IR_mapping.ino
@@ -1,15 +1,15 @@
 #include <IRremote.h>
 
-const int receive_pin=2;
-IRrecv irrecv(receive_pin);
-decode_results results;
+const int receive_pin=2;// initializing a pin
+IRrecv irrecv(receive_pin); //pulling in values from the initialized pin
+decode_results results; // declaring a variable
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(9600); 
     irrecv.enableIRIn();
 }
 void loop() {
-    if (irrecv.decode(&results)){
+    if (irrecv.decode(&results)){     //setting up an empty loop to keep putting in hexadecimal values into the serial monitor
         Serial.println(results.value,HEX);
         irrecv.resume();  
     }
